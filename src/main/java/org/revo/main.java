7ccda0f977d28ca4624ae1bd9713662b45931ba9
ashraf <ashraf1abdelrasool@gmail.com>
2015-10-15 22:22:59 +0200

    sssss

--- src/main/java/org/revo/main.java
+++ src/main/java/org/revo/main.java
@@ -10,31 +10,58 @@ public class main {
     final static String queue = "revo";
     final static String username = "gcdwsjsu";
     final static String password = "Z14IKszdYSvdMyb9N3ZkFvISSqIlnyg9";
+    private static Channel channel;
+    private static Connection connection;
 
-
-    public static void main(String[] argv) {
+    public static ConnectionFactory getFactory() {
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost(host);
         factory.setUsername(username);
         factory.setVirtualHost(username);
         factory.setPassword(password);
+        return factory;
+    }
 
+    static void closeConnection() {
         try {
-            Connection connection = factory.newConnection();
-            Channel channel = connection.createChannel();
-            Consumer consumer = new DefaultConsumer(channel) {
+            channel.close();
+            connection.close();
+        } catch (IOException e) {
+        } catch (TimeoutException e) {
+        }
+    }
+
+    static void SendMesage(String queuename, byte[] message) {
+        try {
+            channel.basicPublish("", queuename, null, message);
+        } catch (IOException e) {
+        }
+    }
+
+    static Channel createConnection() {
+        try {
+            connection = getFactory().newConnection();
+            channel = connection.createChannel();
+            channel.basicConsume(queue, true, new DefaultConsumer(channel) {
                 @Override
                 public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body)
                         throws IOException {
                     String message = new String(body, "UTF-8");
                     System.out.println(message);
                 }
-            };
-            channel.basicConsume(queue, true, consumer);
-
-        } catch (IOException e) {
+            });
+        } catch (TimeoutException | IOException e) {
             System.out.println(e.getMessage());
-        } catch (TimeoutException e) {
         }
+
+
+//        Observable.interval(1000, TimeUnit.MILLISECONDS).subscribe(i -> {
+//            SendMesage("toServer", "revo".getBytes());
+//        });
+        return channel;
+    }
+
+    public static void main(String[] args) {
+        createConnection();
     }
 }
